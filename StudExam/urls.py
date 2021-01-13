318505740856320d6d59394444a812f455214f10
Vlad Gerasimov <gerasimov.vlad2012@gmail.com>
2021-01-14 00:40:48 +0300

    add demo, fix bugs

--- StudExam/urls.py
+++ StudExam/urls.py
@@ -27,6 +27,7 @@ from Profiles import views as views4
 urlpatterns = [
     path('', views.index, name='home'),
     # path('sign_in/', views.sign_in, name='sign_in'),
+    path('demo/', views.demo, name='demo'),
     path('logout/', views.logout_view, name='logout'),
     path('create_test/', views1.create_test, name='create_test'),
     path('web_testing/', views2.web_testing, name='web_testing'),
